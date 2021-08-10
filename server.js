4e0e8707dede219596e72338df59960cc126fcff
Weicheng Lin <weicheng.lin@seamoney.com>
2021-08-11 01:35:07 +0800

    error handling

--- server.js
+++ server.js
@@ -25,29 +25,65 @@ app.get("/api/hello", function (req, res) {
 });
 
 
-function tryParseDateStr(dateStr) {
+function validateTimestamp(str) {
+  const parsed = parseInt(str, 10);
+  return !isNaN(parsed)
+}
+
+function validateYYYYMMDD(dateStr) {
   var parts = dateStr.split("-");
-  if (parts.length < 3) {
-    return null
+  if (parts.length !== 3) {
+    return false
   }
-  var dt = new Date(
+  let yyyy = parseInt(parts[0], 10)
+  let mm = parseInt(parts[1], 10)
+  let dd = parseInt(parts[2], 10)
+  if (isNaN(yyyy) || isNaN(mm) || isNaN(dd)) {
+    return false
+  }
+  if (yyyy >= 0 && mm >= 1 && mm <= 12 && dd >= 1 && dd <= 31) {
+    return true
+  }
+  return false
+}
+
+function parseYYYYMMDD(str){
+  var parts = str.split("-");
+  return new Date(
     parseInt(parts[0], 10),
     parseInt(parts[1], 10) - 1,
-    parseInt(parts[2], 10));
-  return dt
+    parseInt(parts[2], 10))
+}
+
+function tryParseDateStr(dateStr) {
+  if (validateYYYYMMDD(dateStr)) {
+    var dt = parseYYYYMMDD(dateStr);
+    return [dt, null]
+  } else if (validateTimestamp(dateStr)) {
+    return [null, null]
+  } else {
+    return [null, 'Invalid Date']
+  }
 }
 
 // my second API endpoint...
 app.get("/api/:dateStr", function (req, res) {
-  var dt = tryParseDateStr(req.params.dateStr)
-  if (dt !== null) {
-    res.json({unix: dt.getTime(), utc: dt.toUTCString()});
+  var parseRes = tryParseDateStr(req.params.dateStr)
+  var dt = parseRes[0]
+  var err = parseRes[1]
+  if (err !== null) {
+    res.json({ error : err })
     return
   }
-  var date = new Date(req.params.dateStr * 1);
-  res.json({unix: parseInt(req.params.dateStr), utc: date.toUTCString()});
+  if (dt == null) {
+    dt = new Date(req.params.dateStr * 1);
+  }
+  if (dt.toUTCString() === 'Invalid Date') {
+    res.json({ error : 'Invalid Date' })
+    return
+  }
+  res.json({unix: dt.getTime(), utc: dt.toUTCString()});
 })
-
 
 
 // listen for requests :)
